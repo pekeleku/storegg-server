d1c8c9fcd7529ca6dcb89aad0bebf222f5951947
pekeleku <alumanwalter@gmail.com>
2023-05-02 14:51:03 +0800

    CRUD category => view

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var createError = require("http-errors");
-var express = require("express");
+const express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var categoryRouter = require("./app/category/router");
-
-var app = express();
+const methodOverride = require("method-override");
+const dashboardRouter = require("./app/dashboard/router");
+const categoryRouter = require("./app/category/router");
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -22,7 +22,8 @@ app.use(
   express.static(path.join(__dirname, "/node_modules/admin-lte"))
 );
 
-app.use("/", categoryRouter);
+app.use("/", dashboardRouter);
+app.use("/category", categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app/category/controller.js
+++ app/category/controller.js
@@ -1,11 +1,68 @@
+const Category = require("./model.js");
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", {
-        title: "expressjs",
+      const category = await Category.find();
+      res.render("admin/category/view_category", {
+        category,
       });
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/category/create");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { name } = req.body;
+
+      let category = await Category({ name });
+      await category.save();
+      console.log(name);
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOne({ _id: id });
+      console.log(category);
+      res.render("admin/category/edit", {
+        category,
+      });
+    } catch (error) {}
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      const category = await Category.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        { name }
+      );
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOneAndRemove({
+        _id: id,
+      });
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
     }
   },
 };
