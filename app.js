7cc2f385cb0eec3854685df252378b6138387581
pekeleku <alumanwalter@gmail.com>
2023-05-02 23:40:36 +0800

    CRUD nominal => view

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require("connect-flash");
 
 const dashboardRouter = require("./app/dashboard/router");
 const categoryRouter = require("./app/category/router");
+const nominalRouter = require("./app/nominal/router");
 const app = express();
 
 // view engine setup
@@ -37,6 +38,7 @@ app.use(
 
 app.use("/", dashboardRouter);
 app.use("/category", categoryRouter);
+app.use("/nominal", nominalRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ app/nominal/controller.js
@@ -0,0 +1,93 @@
+const Nominal = require("./model.js");
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+
+      const alert = { message: alertMessage, status: alertStatus };
+      const nominal = await Nominal.find();
+
+      res.render("admin/nominal/view_nominal", {
+        nominal,
+        alert,
+      });
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/category");
+    }
+  },
+  viewCreate: async (req, res) => {
+    try {
+      res.render("admin/nominal/create");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+  actionCreate: async (req, res) => {
+    try {
+      const { coinName, price, coinQuantity } = req.body;
+      let nominal = await Nominal({ coinName, price, coinQuantity });
+      await nominal.save();
+
+      req.flash("alertMessage", "Berhasil Tambah Nominal");
+      req.flash("alertStatus", "success");
+      res.redirect("/nominal");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const nominal = await Nominal.findOne({ _id: id });
+
+      res.render("admin/nominal/edit", {
+        nominal,
+      });
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { coinName, price, coinQuantity } = req.body;
+      await Nominal.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        { coinName, price, coinQuantity }
+      );
+      req.flash("alertMessage", "Berhasil Ubah Nominal");
+      req.flash("alertStatus", "success");
+      res.redirect("/nominal");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Nominal.findOneAndRemove({
+        _id: id,
+      });
+      req.flash("alertMessage", "Berhasil Hapus Nominal");
+      req.flash("alertStatus", "success");
+      res.redirect("/nominal");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/nominal");
+    }
+  },
+};
